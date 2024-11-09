73bee53c1a27dd100f8e2b246cb47be79b3a498c
Akash Kolla <ak654063@dal.ca>
2024-11-09 13:39:00 -0400

    Container1

--- app.js
+++ app.js
@@ -71,4 +71,4 @@ app.post('/calculate', async (req, res) => {
 app.listen(PORT, () => {
     console.log(`Frontend service running on port ${PORT}`);
 });
-//K8s assignment
+//K8s assignment- CSCI 5409 Akash
